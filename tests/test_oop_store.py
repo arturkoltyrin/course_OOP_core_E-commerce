8c9c663ef7fd2f6ec9c2bf7b81d77d804bf2e196
Artur Koltyrin <koltyrinartur@gmail.com>
2024-11-08 19:30:30 +0500

    Выполнена оценка покрытия тестами

--- tests/test_oop_store.py
+++ tests/test_oop_store.py
@@ -3,7 +3,9 @@ from commerce_cours_oop.oop_store import Product, Category
 
 
 def test_product_initialization():
-    product = Product(name="Laptop", description="A high-performing laptop", price=999.99, quantity=10)
+    product = Product(
+        name="Laptop", description="A high-performing laptop", price=999.99, quantity=10
+    )
     assert product.name == "Laptop"
     assert product.description == "A high-performing laptop"
     assert product.price == 999.99
@@ -19,15 +21,16 @@ def test_category_initialization():
 
 def test_product_count():
     category = Category(name="Electronics", description="All electronic items")
-    product1 = Product(name="Laptop", description="A high-performing laptop", price=999.99, quantity=10)
-    product2 = Product(name="Smartphone", description="A smartphone with great features", price=499.99, quantity=20)
+    product1 = Product(
+        name="Laptop", description="A high-performing laptop", price=999.99, quantity=10
+    )
+    product2 = Product(
+        name="Smartphone",
+        description="A smartphone with great features",
+        price=499.99,
+        quantity=20,
+    )
     category.add_product(product1)
     category.add_product(product2)
     assert len(category.products) == 2
     assert Category.total_products == 2
-
-
-def test_category_count():
-    category1 = Category(name="Electronics", description="All electronic items")
-    category2 = Category(name="Clothing", description="All clothing items")
-    assert Category.total_categories == 2
